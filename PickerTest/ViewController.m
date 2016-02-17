ee02af82ff427073c71f91227cc525ae66ece221
xcode8 <529404579@qq.com>
2016-02-17 14:36:30 +0800

    修复选择钓鱼岛闪退问题。 代码优化！

--- PickerTest/ViewController.m
+++ PickerTest/ViewController.m
@@ -38,6 +38,35 @@
     
     [super viewDidLoad];
 }
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    
+    //获取中国所有城市
+    [self getChinaCitysAndCounty];
+    
+    UIButton * BtnView = [[UIButton alloc]init];
+    
+    BtnView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.2];
+    
+    [BtnView addTarget:self action:@selector(BtnTap:) forControlEvents:UIControlEventTouchDown];
+    
+    [self.view addSubview:BtnView];
+    
+    
+    picker = [[UIPickerView alloc]initWithFrame:CGRectMake(0, self.view.frame.size.height, self.view.frame.size.width, 200)];
+    picker.delegate = self;
+    picker.dataSource = self;
+    // picker.backgroundColor=[UIColor orangeColor];
+    
+    [self.view addSubview:picker];
+    
+    [UIView animateWithDuration:0.8 animations:^{
+        
+        BtnView.frame = CGRectMake(0,0, self.view.frame.size.width, self.view.frame.size.height);
+        picker.frame = CGRectMake(10, self.view.center.y-150, self.view.frame.size.width-20, 200);
+    }];
+}
 
 //获取中国所有城市
 -(void)getChinaCitysAndCounty
@@ -82,29 +111,29 @@
     [self getCountys:0];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
 -(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
     return 3;
 }
+
 -(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
-    switch (component) {
+    switch (component)
+    {
         case 0:
             return  provinces.count;
             break;
         case 1:
             return  citys.count;
             break;
-        case 2:
-            return  countys.count;
-            break;
         default:
+            return  countys.count-1;
             break;
     }
     return YES;
     
 }
-
 - (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
     switch (component) {
             
@@ -113,19 +142,19 @@
             break;
             
         case 1:
-           
-                return [NSString stringWithFormat:@"%@",citys[row]];
-            break;
             
-        case 2:
-            
-            return [NSString stringWithFormat:@"%@",countys[row]];
+            return [NSString stringWithFormat:@"%@",citys[row]];
             break;
             
         default:
-            return @"";
+            return [NSString stringWithFormat:@"%@",countys[row]];
             break;
     }
+}
+
+-(CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component
+{
+    return 20.0;
 }
 
 - (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view{
@@ -138,12 +167,16 @@
         pickerLabel.adjustsFontSizeToFitWidth = YES;
         [pickerLabel setTextAlignment:NSTextAlignmentCenter];
         [pickerLabel setBackgroundColor:[UIColor clearColor]];
-        [pickerLabel setFont:[UIFont boldSystemFontOfSize:15]];
+        [pickerLabel setFont:[UIFont boldSystemFontOfSize:15.0]];
     }
     
     pickerView.tintColor = [UIColor blackColor];
     
     pickerLabel.text=[self pickerView:pickerView titleForRow:row forComponent:component];
+    
+    [[picker.subviews objectAtIndex:1] setHidden:TRUE];
+    [[picker.subviews objectAtIndex:2] setHidden:TRUE];
+    
     
     return pickerLabel;
 }
@@ -152,6 +185,7 @@
 {
     switch (component) {
             
+            
         case 0:
         {
             [citys removeAllObjects];
@@ -159,12 +193,13 @@
             [countys removeAllObjects];
             
             [countysTmp removeAllObjects];
-           
+            
+            //获取当前省所有市
             //获取当前省所有市
             NSMutableArray * currenCity = [self getCityAndCounty:row];
             //获取当前市所有区和县
             NSMutableArray * currenCounty = [self getCountys:0];
-           
+            
             [pickerView reloadComponent:1];
             [pickerView reloadComponent:2];
             
@@ -177,46 +212,70 @@
             city = currenCity[0];
             
             county = currenCounty[0];
+            
             break;
         }
             
         case 1:
         {
             //获得市的县和区
-           NSMutableArray * currenCounty = [self getCountys:row];
+            NSMutableArray * currenCounty = [self getCountys:row];
             
-            city = pickerLabel.text;
-            
-            county = currenCounty[1];
+            if (row > 0)
+            {
+                [pickerView selectRow:1 inComponent:2 animated:YES];
+            }
+            else
+            {
+                [pickerView selectRow:0 inComponent:2 animated:YES];
+            }
             
             [pickerView reloadComponent:2];
             
-            [pickerView selectRow:1 inComponent:2 animated:YES];
+            city = citys[row];
+            
+            county = currenCounty[row];
             
             break;
         }
         default:
-            county = pickerLabel.text;
+            county = countys[row];
             break;
     }
+    NSLog(@"%@/%@/%@",province,city,county);
 }
+
 //得到当前县和区
 -(NSMutableArray *)getCountys:(NSInteger )_row
 {
     //把当前市的所有区和县转换为字符串
     NSString * countyStr= [countysTmp componentsJoinedByString:@","];
+    NSMutableString * delteStr = [NSMutableString stringWithString:countyStr] ;
     
-    //  NSLog(@"%@",countyStr);
+    //NSLog(@"%@",countyStr);
+    
+    //把转换好的字符串再转回数组（通过“/”间隔元素）
+    NSArray * countyArrTmp = [delteStr componentsSeparatedByString:NSLocalizedString(@"/", nil)];
+    
+    for (int i = 0; i < countyArrTmp.count; i++)
+    {
+        [self deleteStr:delteStr andToStr:@",/," andBOOL:YES andInsert:@"/"];
+        [self deleteStr:delteStr andToStr:@",/" andBOOL:NO andInsert:@""];
+    }
+    
     //把转换好的字符串再转回数组（通过“/”间隔元素）
     NSArray * countyArr = [countyStr componentsSeparatedByString:NSLocalizedString(@"/", nil)];
-  //  NSLog(@"%lu",(unsigned long)countyArr.count);
- 
+    
+    // NSLog(@"%@",countyArr);
+    
+    // NSLog(@"%@",delteStr);
+    
     NSArray * countyTmpArr = [countyArr[_row] componentsSeparatedByString:@","];
-   
+    
     [countys removeAllObjects];
     
     //把转换后的县和区赋值给保存县和区的数组
-    countys =(NSMutableArray *) countyTmpArr;
+    countys = (NSMutableArray *) countyTmpArr;
     
     return  countys;
 }
@@ -232,7 +291,7 @@
         //获取当前省所有市
         [ citys addObject:cityStr];
         
-        //NSLog(@"%@",citys);
+        // NSLog(@"%@",citys);
         
         //获取北京直辖市所有区和县
         NSArray * countyArr = [NSArray arrayWithObjects:[dict objectForKey:@"area"], nil];
@@ -246,7 +305,7 @@
                 
                 [countysTmp addObject:countyStr];
                 
-                if (i == ArrCounty.count-1) {
+                if (i == ArrCounty.count) {
                     
                     [countysTmp addObject:@"/"];
                 }
@@ -255,14 +314,21 @@
             }
             
         }
-        
-        
     }
     return citys;
 }
 
-//删除和替换字符串
--(NSString *)deleteStr:(NSMutableString *)str_1 andToStr:(NSString *)str_2 andStrLength:(NSInteger )_length;
+/**
+ *  替换字符串/删除字符串
+ *
+ *  @param str_1 查找要被替换掉或删除的字符串
+ *  @param str_2 要被替换的字符串
+ *  @param b     B=Yes替换/B=No删除
+ *  @param str_3 如果B=Yes（要替换的新字符内容）
+ *
+ *  @return 最后以字符串形式返回
+ */
+-(NSString *)deleteStr:(NSMutableString *)str_1 andToStr:(NSString *)str_2 andBOOL:(BOOL)b andInsert:(NSString *)str_3;
 {
     
     NSRange range =[str_1 rangeOfString:str_2];
@@ -273,39 +339,19 @@
     {
         NSRange deleage={range.location,range.length};
         
-        [str_1 deleteCharactersInRange:deleage];
+        if (!b)
+        {
+            [str_1 deleteCharactersInRange:deleage];
+        }
+        else
+        {
+            [str_1 deleteCharactersInRange:deleage];
+            [str_1 insertString:str_3 atIndex:range.location];
+            
+        }
     }
     
     return str_1;
-}
--(void)textFieldDidBeginEditing:(UITextField *)textField
-{
-    [textField resignFirstResponder];
-    
-    //获取中国所有城市
-    [self getChinaCitysAndCounty];
-    
-    UIButton * BtnView = [[UIButton alloc]init];
-    
-    BtnView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.2];
-    
-    [BtnView addTarget:self action:@selector(BtnTap:) forControlEvents:UIControlEventTouchDown];
-    
-    [self.view addSubview:BtnView];
-    
-    
-    picker = [[UIPickerView alloc]initWithFrame:CGRectMake(0, self.view.frame.size.height, self.view.frame.size.width, 200)];
-    picker.delegate = self;
-    picker.dataSource = self;
-   // picker.backgroundColor=[UIColor orangeColor];
-    
-    [self.view addSubview:picker];
-    
-    [UIView animateWithDuration:0.8 animations:^{
-        
-        BtnView.frame = CGRectMake(0,0, self.view.frame.size.width, self.view.frame.size.height);
-        picker.frame = CGRectMake(10, self.view.center.y-150, self.view.frame.size.width-20, 200);
-    }];
 }
 
 -(void)BtnTap:(UIButton *)sender
